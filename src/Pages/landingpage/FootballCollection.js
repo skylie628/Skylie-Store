3bf819679498614a5696009d8eabbe9004ae9a9c
skylie628 <khuong.dinhbkk17@hcmut.edu.vn>
2023-02-18 12:35:09 +0800

    first landing pages

--- src/Pages/landingpage/FootballCollection.js
+++ src/Pages/landingpage/FootballCollection.js
@@ -1,68 +1,93 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import styles from './footballcollection.module.css'
+import styles from './footballcollection.module.css';
+import useWindowDimensions from "../../Hooks/useWindowDimensions";
 import { useEffect, useState } from "react";
 import footballCollection1 from  '../../assets/images/football-collection-1.png'
 import footballCollection2 from  '../../assets/images/football-collection-2.png'
 import footballCollection3 from  '../../assets/images/football-collection-3.png'
 import footballCollection4 from  '../../assets/images/football-collection-4.png'
 export default function FootballCollection() {
-  const {current, setCurrent} = useState(1)
-  const {disableTransition,setDisableTransition} = useState(false)
+  const [cur, setCur] = useState(1);
+  //const [speed,setSpeed] = useState(2000)
+  let speed = 2000;
+  const [disableTransition,setDisableTransition] = useState(false);
+  //const { height, width } = useWindowDimensions();
+  const width = 1263;
+  let timer;
+  const updateCur = () =>{
+    timer = !timer && setInterval(()=>{
+      setCur(preCur => {
+        if(preCur == 5){
+          setDisableTransition(true);
+          return 1;
+        }
+        else{
+          speed=2000;
+          setDisableTransition(false);
+          console.log(disableTransition)
+          return preCur +1 ;
+        }
+      });
+    },speed)
+  }
   const goback = () =>{
-    if(current == 1){
+    if(cur == 1){
       setDisableTransition(true);
-      setCurrent(5);
+      setCur(5);
       goback();
     }
     else{
       if(disableTransition){
         setDisableTransition(false)
       }
-      setCurrent((current)=> current-1)
+      setCur((cur)=> cur-1)
     }
   }
   const goforward = () =>{
-    if(current == 5){
+    if(cur == 5){
       setDisableTransition(true);
-      setCurrent(1);
+      setCur(1);
       goforward();
     }
     else{
+      console.log(cur)
       if(disableTransition){
         setDisableTransition(false)
       }
-      setCurrent((current)=> current+1)
+      setCur(precur => precur +1 )
     }
   }
   useEffect(() => {
-    const interval = setInterval(() => {
+   /* const interval = setInterval(() => {
+      console.log(width)
       goforward()
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+    }, 5000); */
+    updateCur()
+  },[cur]);
   return (
     <div className = {styles.footballCollectionWrapper}>
-    <div className={styles.footballCollectionContentWrapper }>
+    <div>{cur }aa</div>
+    <div className= {`${styles.footballCollectionContentWrapper} ${!disableTransition && styles.transition}`}  style = {{right : `${0.85*width + 30 + (cur-1)*(0.9*width + 20)}px`}}>
         <div className={styles.footballCollectionImageWrapper }>
-        <img src = {footballCollection4} className = {styles.footballCollectionImg} ></img>
+        <img src = {footballCollection4} className = {`${styles.footballCollectionImg} ${styles.outfocus}`} ></img>
         </div>
         <div className={styles.footballCollectionImageWrapper }>
-        <img src = {footballCollection1} className = {styles.footballCollectionImg} ></img>
+        <img src = {footballCollection1} className = {`${styles.footballCollectionImg} ${(cur == 1)? '': styles.outfocus } `} ></img>
         </div>
         <div className={styles.footballCollectionImageWrapper }>
-        <img src = {footballCollection2} className = {styles.footballCollectionImg} ></img>
+        <img src = {footballCollection2} className = {`${styles.footballCollectionImg} ${(cur == 2)? '': styles.outfocus } `} ></img>
         </div>
         <div className={styles.footballCollectionImageWrapper }>
-        <img src = {footballCollection3} className = {styles.footballCollectionImg} ></img>
+        <img src = {footballCollection3} className = {`${styles.footballCollectionImg} ${(cur === 3)? '': styles.outfocus } `} ></img>
         </div>
         <div className={styles.footballCollectionImageWrapper }>
-        <img src = {footballCollection4} className = {styles.footballCollectionImg} ></img>
+        <img src = {footballCollection4} className = {`${styles.footballCollectionImg} ${(cur === 4)? '': styles.outfocus } `} ></img>
         </div>
         <div className={styles.footballCollectionImageWrapper }>
-        <img src = {footballCollection1} className = {styles.footballCollectionImg} ></img>
+        <img src = {footballCollection1} className = {`${styles.footballCollectionImg} ${(cur === 5)? '': styles.outfocus } `} ></img>
         </div>
         <div className={styles.footballCollectionImageWrapper }>
-        <img src = {footballCollection2} className = {styles.footballCollectionImg} ></img>
+        <img src = {footballCollection2} className = {`${styles.footballCollectionImg} ${(cur === 6)? '': styles.outfocus } `} ></img>
          </div>
        </div>
     </div>
