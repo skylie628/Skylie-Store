7635472d0ceba678aea07ee037044cc6d79424fa
skylie628 <khuong.dinhbkk17@hcmut.edu.vn>
2023-06-11 01:41:10 +0800

    finish deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
   <React.StrictMode>
-    <BrowserRouter  basename="/">
+    <BrowserRouter  basename="/index.html">
     <Provider store ={store} >
       <PersistGate loading = {null} persistor = {persistor}>
     <App />
